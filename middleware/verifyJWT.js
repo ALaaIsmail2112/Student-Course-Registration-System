732c05f85fc771f2a6af7db1cfe558097ee15fd9
Alaa Ismail <alaaismailmohamed144@gmail.com>
2025-07-27 22:52:17 +0300

    Add files via upload

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+
+const verifyjwt = (req, res, next) =>{
+     const authHeader = req.headers.authorization || req.headers.Authorization  
+     if (!authHeader?.startsWith('Bearer')) return res.sendStatus(401)
+     console.log(authHeader) // Bearer token
+     const token = authHeader.split(' ')[1]
+     jwt.verify(
+          token,
+          process.env.ACCESS_TOKEN_SECRET,
+          (err , decoded) =>{
+               if (err) return res.sendStatus(403) // invalid token // forbidden
+               req.user = decoded.UserInfo.username
+               req._id = decoded.UserInfo._id
+               req.roles = decoded.UserInfo.roles
+               next()
+          }
+
+     )
+}
+
+module.exports = verifyjwt
